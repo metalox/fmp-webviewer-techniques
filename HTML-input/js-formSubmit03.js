8dd063de6ce2b67b3d0d62acb981a4fc81d9530f
metalox <benedick.miller@web.de>
2021-05-16 19:13:48 +0200

    Create js-formSubmit03.js

---
+++ HTML-input/js-formSubmit03.js
@@ -0,0 +1,19 @@
+var fmScriptEdit="edit person OK";
+
+function handleSubmit(event) {
+event.preventDefault();
+
+const data = new FormData(event.target);
+
+
+const value = Object.fromEntries(data.entries());
+
+value.prefs = data.getAll("prefs");
+
+const result = JSON.stringify(value);
+
+FileMaker.PerformScriptWithOption ( fmScriptEdit, result , "0" );
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', handleSubmit);
