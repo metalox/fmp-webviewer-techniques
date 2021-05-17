395a251c92a9d994ee3b33784972e80324aac941
metalox <benedick.miller@web.de>
2021-05-17 19:52:16 +0200

    Update js-formSubmit03.js
    added hashBefore data

--- HTML-input/js-formSubmit03.js
+++ HTML-input/js-formSubmit03.js
@@ -1,17 +1,16 @@
 var fmScriptEdit="edit person OK";
+const recordinfo = document.getElementById("recordinfo");
+const id_people = recordinfo.getAttribute("data-id_people");
+const hashBefore = recordinfo.getAttribute("data-hashBefore");
 
 function handleSubmit(event) {
 event.preventDefault();
-
 const data = new FormData(event.target);
-
-
-const value = Object.fromEntries(data.entries());
-
-value.prefs = data.getAll("prefs");
-
-const result = JSON.stringify(value);
-
+var formData = Object.fromEntries(data.entries());
+formData.prefs = data.getAll("prefs");
+formData.id_people = id_people ;
+formData.hashBefore = hashBefore ;
+const result = JSON.stringify( formData );
 FileMaker.PerformScriptWithOption ( fmScriptEdit, result , "0" );
 }
 
