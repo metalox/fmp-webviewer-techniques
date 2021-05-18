cc49f383ebd47025c59d47c26f5dd8b0857a88dd
metalox <benedick.miller@web.de>
2021-05-18 21:12:49 +0200

    updated submit js

--- HTML-input/js-formSubmit03.js
+++ HTML-input/js-formSubmit03.js
@@ -1,9 +1,36 @@
-var fmScriptEdit="edit person OK";
+// to do - deal with [Enter] better?
+// [Enter] shlould only submit when the submit button is in focus
+// But Enter should work as Return in the notes test area
+// and maybe Enter should set the value from a dropdown - without submitting - ?
+
+const fmScriptEdit="edit person OK";
+//const form = document.querySelector('form');
+const form = document.getElementById("form1");
+const notes = document.getElementById("notes");
+const cancelbtn = document.getElementById("cancelbtn");
+const submitbtn = document.getElementById("submitbtn");
 const recordinfo = document.getElementById("recordinfo");
 const id_people = recordinfo.getAttribute("data-id_people");
 const hashBefore = recordinfo.getAttribute("data-hashBefore");
 const newRecord = recordinfo.getAttribute("data-newRecord");
 
+form.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+}, true);
+
+form.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+}, true);
+
+function noEnter() {
+form.setAttribute ("onkeydown" , "return event.key != 'Enter';" );
+};
+
+function allowEnter() {
+form.removeAttribute("onkeydown");
+};
+
+// on submit
 function handleSubmit(event) {
 event.preventDefault();
 const data = new FormData(event.target);
@@ -14,7 +41,18 @@ formData.hashBefore = hashBefore ;
 formData.newRecord = newRecord  ;
 const result = JSON.stringify( formData );
 FileMaker.PerformScriptWithOption ( fmScriptEdit, result , "0" );
+};
+
+// cancel
+function cancel() {
+FileMaker.PerformScriptWithOption ( fmScriptEdit, "cancel" , "0" );
 }
 
-const form = document.querySelector('form');
+cancelbtn.addEventListener('click', cancel);
+notes.addEventListener('focus', allowEnter);
+// notes.addEventListener('blur', noEnter);
+submitbtn.addEventListener('focus', allowEnter);
+submitbtn.addEventListener('blur', noEnter);
 form.addEventListener('submit', handleSubmit);
+
+
