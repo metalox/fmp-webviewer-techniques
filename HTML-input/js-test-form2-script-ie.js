832422a41ef52bfe01d01a784a6211d5fd50ab27
metalox <benedick.miller@web.de>
2021-05-20 15:39:18 +0200

    testing jquery
    using jquery for browser compatibility - for windows explorer 11

--- HTML-input/js-test-form2-script-ie.js
+++ HTML-input/js-test-form2-script-ie.js
@@ -1,7 +1,15 @@
 
-$( "form" ).submit(function( event ) {
-    alert( $( this ).serializeArray() );
-    event.preventDefault();
+function showValues() {
+  var fields = $( ":input" ).serializeArray();
+  $( "#results" ).empty();
+  jQuery.each( fields, function( i, field ) {
+    $( "#results" ).append( field.value + " " );
   });
+}
+
+$( ":checkbox, :radio" ).click( showValues );
+$( "select" ).change( showValues );
+showValues();
+
 
 
