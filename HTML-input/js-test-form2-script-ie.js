05eebc95eb0dc05e5743e77c191bb64888ba537f
metalox <benedick.miller@web.de>
2021-05-20 23:43:29 +0200

    jquery script updates

--- HTML-input/js-test-form2-script-ie.js
+++ HTML-input/js-test-form2-script-ie.js
@@ -1,9 +1,30 @@
 
 
+//$( "form" ).submit(function( event ) {
+//  event.preventDefault();
+//  const myFormArray = $( this ).serializeArray();
+//  var myObject = {};
+//  myObject.formData = myFormArray;
+//  alert(JSON.stringify(myObject));
+// });
+
+$.fn.getFormValues = function(){
+  var formvals = {};
+  $.each($(':input',this).serializeArray(),function(i,obj){     
+      if (formvals[obj.name] == undefined)
+          formvals[obj.name] = obj.value;
+      else if (typeof formvals[obj.name] == Array) 
+          formvals[obj.name].push(obj.value);
+      else formvals[obj.name] = [formvals[obj.name],obj.value];
+  }); 
+  return formvals;
+}
+
 $( "form" ).submit(function( event ) {
-  event.preventDefault();
-  const myObject = $( this ).serializeArray();
-  alert(JSON.stringify(myObject));
+ event.preventDefault();
+ var myarr = $('#form1').getFormValues();
+alert(JSON.stringify(myarr));
 });
+
 
 
