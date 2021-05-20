0b24b5f019f7a35d0e7875cf2bccbbe91b1c82e2
metalox <benedick.miller@web.de>
2021-05-20 18:40:29 +0200

    updated test script

--- HTML-input/js-test-form2-script-ie.js
+++ HTML-input/js-test-form2-script-ie.js
@@ -1,19 +1,9 @@
 
 
-
-function showValues() {
-  var fields = $( ":input" ).serializeArray();
-  $( "#results" ).empty();
-  jQuery.each( fields, function( i, field ) {
-    $( "#results" ).append( field.value + " " );
-  });
-}
-
-
-
-$( ":checkbox, :radio" ).click( showValues );
-$( "select" ).change( showValues );
-showValues();
-
+$( "form" ).submit(function( event ) {
+  var myObject = $( this ).serializeArray();
+  event.preventDefault();
+  alert(JSON.stringify(myObject));
+});
 
 
